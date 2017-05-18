ca6f4799b89530dcff9a6f5e5a426c2f5b6f3dfb
Bill Hoang <billhoang79@gmail.com>
2017-05-17 20:37:34 -0700

    Update main.js

--- queues/js/main.js
+++ queues/js/main.js
@@ -1,46 +1,50 @@
-// Priority Queue
+// Queue Class
 
-function PriorityQueue(){
+function Queue(){
 
 	var items = [];
 
-	function QueueElement(element, priority, isEmpty){
-		this.element = element;
-		this.priority = priority;
-		this.isEmpty = isEmpty;
-	}
+	this.enqueue = function(element){
+		items.push(element);
+	};
+
+	this.dequeue = function(){
+		return items.shift();
+	};
+
+	this.front = function(){
+		return items[0];
+	};
 
 	this.isEmpty = function(){
 		return items.length == 0;
-	}
-
-	this.enqueue = function(element, priority){
-		var queueElement = new QueueElement(element, priority);
-
-		if (this.isEmpty()){
-			items.push(queueElement);
-		} else {
-			var added = false;
-			for (var i=0; i<items.length; i++){
-				if(queueElement.priority < items[i].priority){
-					items.splice(i,0,queueElement);
-					added = true;
-					break; 
-				}
-			}
-			if (!added){
-				items.push(queueElement);
-			}
-		}
 	};
 
-	var priorityQueue = new PriorityQueue();
+	this.clear = function(){
+		items = [];
+	};
 
-	priorityQueue.enqueue("John", 2);
-	priorityQueue.enqueue("Jack", 1);
-	priorityQueue.enqueue("Camila", 1);
-	priorityQueue.print();
+	this.size = function(){
+		return items.length;
+	};
 
-	console.log(priorityQueue);
+	this.print = function(){
+		console.log(items.toString());
+	};
 }
+
+var queue = new Queue();
+console.log(queue.isEmpty());
+
+queue.enqueue("John");
+queue.enqueue("Jack");
+queue.enqueue("Camila");
+
+queue.print();
+console.log(queue.size());
+console.log(queue.isEmpty());
+queue.dequeue();
+queue.dequeue();
+queue.print();
+
 
